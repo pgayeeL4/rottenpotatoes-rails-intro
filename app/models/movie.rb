83bb0f59e40959acfd803a640f7b9917ec441038
Pratik G <pgayee@unomaha.edu>
2016-06-20 00:37:22 +0000

    Revert "adding in sorting and ratings"
    This reverts commit 4827c3aa573bb02fc5f0475895d1c286ba803efb.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,2 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        allRatings = []
-        Movie.all.each do |movie|
-            if (allRatings.find_index(movie.rating) == nil)
-                allRatings.push(movie.rating)
-            end
-        end
-        return allRatings
-    end
 end
