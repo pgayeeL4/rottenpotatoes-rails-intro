b55f0100cf2060e018aba5af1097b5a4a3b87ea3
Pratik G <pgayee@unomaha.edu>
2016-06-20 01:35:36 +0000

    Revert "adding in movies controller rb sort"
    This reverts commit 44dd26a1b340ad22beaf631f26d116b8c13c6cca.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,15 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings
-    filtered_ratings = @all_ratings - params[:ratings].keys
-    if (params[:sort] == "title")
-      @movies = Movie.find(:all, :condition => "rating => #{filtered_ratings}", :order => "title")
-    elsif (params[:sort] == "release_date")
-      @movies = Movie.find(:all, :condition => "rating => #{filtered_ratings}", :order => "release_date")
-    elsif (params[:sort] == nil)
-      @movies = Movie.find(:all, :condition => "rating => #{filtered_ratings}")
-    end
+    @movies = Movie.all
   end
 
   def new
@@ -23,7 +19,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -34,7 +30,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
